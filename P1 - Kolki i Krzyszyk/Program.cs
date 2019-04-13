890daa2a48e22b0e9d30e1ccbd65fc6552772302
dominik.markiel <dominik.markiel@PK2-13>
2019-04-13 15:02:45 +0200

    Pierwsza wersja kodu

--- P1 - Kolki i Krzyszyk/Program.cs
+++ P1 - Kolki i Krzyszyk/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace P1___Kolki_i_Krzyszyk
+namespace P1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+            char[,] plansza = new char[3, 3]{
+                {'1', '2', '3' },
+                {'4', '5', '6' },
+                {'7', '8', '9' },
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
+
         }
     }
 }
