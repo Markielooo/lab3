a955d81b98bd0add2f2adcf2c7f31e2fe616fc31
Markiel Dominik <dominik.markiel@microsoft.wsei.edu.pl>
2019-04-13 14:47:04 +0200

    Dodaj pliki projektów.

---
+++ P1 - Kolki i Krzyszyk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1___Kolki_i_Krzyszyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
